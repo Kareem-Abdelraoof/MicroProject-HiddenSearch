62ce4df0fd968717e3f349a3c182c1f6b83b0f00
Kareem Abd-Elraoof <kareemabdelraoof@gmail.com>
2024-02-26 10:19:26 +0200

    there is still a little bug in the transition and thinking about it

---
+++ app.js
@@ -0,0 +1,5 @@
+
+document.querySelector('button').addEventListener('click', e => {
+    // document.querySelector('button').classList.toggle('rightShift');
+    document.querySelector('input').classList.toggle('disappearing')
+})
